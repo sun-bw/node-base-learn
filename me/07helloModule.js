3470a858af3bf61a74d7de86e90a50421acb5d1d
sunbowen <857153966@qq.com>
2020-05-15 16:46:41 +0800

    12312

---
+++ me/07helloModule.js
@@ -0,0 +1,17 @@
+// exports.name = "孙悟空";
+// exports.age = 18;
+// exports.sayName = function () {
+//     console.log('我是孙悟空····')
+// }
+/* 
+基础数据类型存储在栈内存里面
+对象存在在堆内存里面，二进制地址存在栈内存里面
+*/
+module.exports = {
+    name:'asdad',
+    age:13,
+    sayName:function(){
+        console.log('123121212312')
+    }
+}
+console.log(name)
